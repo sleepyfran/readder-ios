244aa13cfd03689e8f87c4c1c2987b423d046b08
Fran González <fgonzaleva@gmail.com>
2017-12-19 12:46:08 +0100

    Little documentation update

--- readder/Networking/RedditApi.swift
+++ readder/Networking/RedditApi.swift
@@ -16,12 +16,11 @@ class RedditApi {
     
     // MARK: Authentication functions.
     
-    // Gets the access token needed to interact with the
+    // Gets the access token needed to interact with the Reddit API. Do not modify anything here except estrictly needed,
+    // all the parameters or endpoint changes should be done in its separate file (ApiParameters or ApiEndpoints).
     static func getAccessToken() -> DataRequest {
         return Alamofire.request(ACCESS_TOKEN_URL,
                                  method: .post,
                                  parameters: ACCESS_TOKEN_PARAMETERS)
     }
-    
-    
 }
